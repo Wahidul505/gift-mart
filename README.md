59c1e99aa90e6ca839ce5a4f7c6070d3d50b797c
Wahidul30 <wahidul505@gmail.com>
2022-03-26 17:30:10 +0600

    final commit

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 ## Gift Mart
 ### Live Site:
 Visit [https://gift-mart.netlify.app/](https://gift-mart.netlify.app/) to see the webpage.
+### Webpage Features:
+* This website is for buying gift .
+* User can select gift item and store in cart section.
+* User can select less than 5 gift and a quantity from each gift card.
+* If User tries to select more than 4 gift a warning modal will pop up.
+* User can delete a selected gift from the cart or reset the whole cart items.
+* In the cart section, A special features added in. User can generate a random item from the selected items.
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,16 +5,23 @@ import Cart from '../Cart/Cart';
 import Modal from '../Modal/Modal';
 
 const Home = () => {
+    // state for storing the products data 
     const [products, setProducts] = useState([]);
+    // state for storing the cart products 
     const [cart, setCart] = useState([]);
+    // state for storing the randomly generated product 
     const [generatedItem, setGeneratedItem] = useState({});
+    // state for displaying the modal 
     const [display, setDisplay] = useState('none');
+    // state for displaying the randomly generated product 
     const [show, setShow] = useState('none');
+    // useEffect for fetching and sets the products data 
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+    // handler for add product into cart 
     const addToCart = (product) =>{
         if(cart.length < 4){
             const exist = cart.find(element => element.id === product.id);
@@ -27,6 +34,7 @@ const Home = () => {
             setDisplay('block');
         }
     }
+    // handler for generate a random product
     const generateCartItem = () =>{
         if(cart.length > 0){
             const index = Math.round(Math.random() * 10);
@@ -40,15 +48,18 @@ const Home = () => {
             setShow('flex');
         }
     }
+    // handler for delete specified item from cart 
     const deleteItem = (product) =>{
         const restItem = cart.filter(item => item.id !== product.id);
         setCart(restItem);
     }
+    // handler for clear the whole cart 
     const resetCart = () =>{
         const emptyCart = [];
         setCart(emptyCart);
         setShow('none');
     }
+    // handler for closing the warning modal 
     const closeModal = () =>{
         setDisplay('none');
     }
